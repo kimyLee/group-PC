6a03244b84279b28fb228f796b947cb0f49399d6
Katherine <1224287934@qq.com>
2017-05-17 11:40:05 +0800

    新增加了任务拖拽模块

--- src/components/DraggableTask.vue
+++ src/components/DraggableTask.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="draggable">
     <div class="task-title">{{task.title}}</div>
-    <draggable>
+    <draggable :options="{group:'task'}" v-model="task.cards">
       <div v-for="taskcard in task.cards" :id="taskcard.id" class="card">{{taskcard.title}}</div>
     </draggable>
   </div>
@@ -20,12 +20,21 @@
     width:250px;
     margin:5px;
     height:100%;
-    background-color: #bbbbbb;
+    background-color: #f1f1f1;
+    padding:10px;
+    border-radius: 5px;
+    .task-title {
+      margin-bottom: 10px;
+      padding-left: 5px;
+      font-weight: bolder;
+    }
     .card {
       max-height:60px;
       padding:10px;
       background-color: white;
       margin:5px;
+      border-radius: 5px;
+      cursor: move;
     }
   }
 </style>
