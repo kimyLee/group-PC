7375d4de573b62ff33a92667a3dc6a43f65a84a8
kimylee <1095643875@qq.com>
2017-05-17 12:18:42 +0800

    拖拽样式

--- src/components/DraggableTask.vue
+++ src/components/DraggableTask.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="draggable">
     <div class="task-title">{{task.title}}</div>
-    <draggable :options="{group:'task'}" v-model="task.cards">
-      <div v-for="taskcard in task.cards" :id="taskcard.id" class="card">{{taskcard.title}}</div>
+    <draggable :options="options" v-model="task.cards">
+      <div v-for="taskcard in task.cards" :id="taskcard.id">
+      <div  class="card">{{taskcard.title}}</div>
+      </div>
     </draggable>
   </div>
 </template>
@@ -10,6 +12,15 @@
   import draggable from 'vuedraggable'
   export default {
     props: ['task'],
+    data () {
+      return {
+        options: {
+          group: 'task',
+          animation: 100,
+          forceFallback: true
+        }
+      }
+    },
     components: {
       draggable
     }
@@ -35,6 +46,19 @@
       margin:5px;
       border-radius: 5px;
       cursor: move;
+    }
+    .sortable-fallback {
+      opacity: 1!important;
+      .card {
+        box-shadow: 6px 6px 15px #999;
+        transform: rotate(3deg) translateZ(0)!important;
+      }
+    }
+    .sortable-ghost {
+      background-color: #ccc;
+      .card {
+        opacity: 0;
+      }
     }
   }
 </style>
